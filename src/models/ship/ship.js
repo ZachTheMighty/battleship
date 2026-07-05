76f163c4a7e990572eed160aeb22ce9d7f3074b3
ZachTheMighty <promoted66@gmail.com>
2026-07-05 11:19:37 +0300

    refactor: removed isSunk() and updated isSunk field with its return directly whenever the ship is hit

--- src/models/ship/ship.js
+++ src/models/ship/ship.js
@@ -2,14 +2,11 @@ export default class {
   constructor(length) {
     this.length = length;
     this.timesHit = 0;
-    this.isSunk = this.isSunk();
+    this.isSunk = false;
   }
 
   hit() {
     this.timesHit++;
-  }
-
-  isSunk() {
-    return this.length === this.timesHit ? true : false;
+    this.isSunk = this.length === this.timesHit ? true : false;
   }
 }
