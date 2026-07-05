b1f943f83c0595c499e8c994409ffbc2d5686048
ZachTheMighty <promoted66@gmail.com>
2026-07-05 10:30:20 +0300

    add: method for gameboard grid

---
+++ src/models/gameboard/gameboard.js
@@ -0,0 +1,15 @@
+class Gameboard {
+  constructor() {
+    this.grid = this.generateGrid();
+  }
+
+  generateGrid() {
+    const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+    let grid = [];
+
+    alphabet.forEach((char) => {
+      for (let i = 1; i < 11; i++) grid.push({ x: char, y: i });
+    });
+    return grid;
+  }
+}
