74cad9430d7cf05dcfc0b5eaf13124b2ab353c9e
ZachTheMighty <promoted66@gmail.com>
2026-07-05 12:30:32 +0300

    refactor: extracted creating grid code into a method

--- src/modules/views/player_view.js
+++ src/modules/views/player_view.js
@@ -3,16 +3,21 @@ export default class {
     this.app = document.createElement("div");
     this.app.classList.add("player");
 
-    this.grid = document.createElement("div");
+    this.grid = this.createGrid();
     this.grid.classList.add("player-grid");
 
+    this.app.append(this.grid);
+    document.body.append(this.app);
+  }
+
+  createGrid() {
+    const grid = document.createElement("div");
     for (let i = 0; i < 10; i++)
       for (let j = 0; j < 10; j++) {
         const block = document.createElement("div");
         block.classList.add("block");
-        this.grid.append(block);
+        grid.append(block);
       }
-    this.app.append(this.grid);
-    document.body.append(this.app);
+    return grid;
   }
 }
