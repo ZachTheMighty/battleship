fbb0b2b3c4df638f3e9546f6e1c3adf7480a4f10
ZachTheMighty <promoted66@gmail.com>
2026-07-06 14:45:03 +0300

    refactor: extracted code that creates grid and coordinates into a seperate module

---
+++ src/modules/views/create_grid.js
@@ -0,0 +1,39 @@
+export default function (gridClass) {
+  const grid = document.createElement("div");
+  const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+
+  for (let i = 1; i < 11; i++)
+    alphabet.forEach((char) => {
+      const block = document.createElement("div");
+      block.classList.add("block");
+      block.classList.add(char);
+      block.classList.add(i);
+      grid.append(block);
+    });
+
+  const alphabetDiv = document.createElement("div");
+  alphabetDiv.classList.add("alphabet");
+  alphabet.forEach((char) => {
+    const charDiv = document.createElement("div");
+    charDiv.classList.add("x-coordinates");
+    charDiv.textContent = char;
+    alphabetDiv.append(charDiv);
+  });
+
+  const numsDiv = document.createElement("div");
+  numsDiv.classList.add("numbers");
+  for (let i = 1; i < 11; i++) {
+    const numDiv = document.createElement("div");
+    numDiv.classList.add("y-coordinates");
+    numDiv.textContent = i;
+    numsDiv.append(numDiv);
+  }
+
+  grid.classList.add(gridClass);
+
+  const wrapper = document.createElement("div");
+  wrapper.classList.add("wrapper");
+  wrapper.append(numsDiv, grid);
+
+  return { alphabetDiv, wrapper };
+}
--- src/modules/views/player_view.js
+++ src/modules/views/player_view.js
@@ -1,34 +1,12 @@
+import createGrid from "./create_grid.js";
+
 export default class {
   constructor() {
     this.app = document.createElement("div");
     this.app.classList.add("player");
 
-    this.alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
-
-    this.alphabetDiv = document.createElement("div");
-    this.alphabetDiv.classList.add("alphabet");
-    this.alphabet.forEach((char) => {
-      const charDiv = document.createElement("div");
-      charDiv.classList.add("x-coordinates");
-      charDiv.textContent = char;
-      this.alphabetDiv.append(charDiv);
-    });
-
-    this.numsDiv = document.createElement("div");
-    this.numsDiv.classList.add("numbers");
-    for (let i = 1; i < 11; i++) {
-      const numDiv = document.createElement("div");
-      numDiv.classList.add("y-coordinates");
-      numDiv.textContent = i;
-      this.numsDiv.append(numDiv);
-    }
-
-    this.grid = this.createGrid();
-    this.grid.classList.add("player-grid");
-
-    this.wrapper = document.createElement("div");
-    this.wrapper.classList.add("wrapper");
-    this.wrapper.append(this.numsDiv, this.grid);
+    const { alphabetDiv, wrapper } = createGrid("player-grid");
+    this.grid = wrapper.childNodes[1];
 
     this.randomizeDiv = document.createElement("div");
     this.randomizeDiv.classList.add("randomize-div");
@@ -39,23 +17,9 @@ export default class {
 
     this.randomizeDiv.append(this.randomizeButton);
 
-    this.app.append(this.alphabetDiv, this.wrapper, this.randomizeDiv);
+    this.app.append(alphabetDiv, wrapper, this.randomizeDiv);
     document.body.append(this.app);
   }
-
-  createGrid() {
-    const grid = document.createElement("div");
-    for (let i = 1; i < 11; i++)
-      this.alphabet.forEach((char) => {
-        const block = document.createElement("div");
-        block.classList.add("block");
-        block.classList.add(char);
-        block.classList.add(i);
-        grid.append(block);
-      });
-    return grid;
-  }
-
   render(blockObject) {
     const blockDiv = Array.from(this.grid.childNodes).find(
       (block) =>
