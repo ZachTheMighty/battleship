139549acbd39d2d67c83427c54a9776428cbd9c2
ZachTheMighty <promoted66@gmail.com>
2026-07-06 11:55:03 +0300

    add: method for placing ships vertically

--- src/modules/building_classes/gameboard/gameboard.js
+++ src/modules/building_classes/gameboard/gameboard.js
@@ -24,11 +24,12 @@ export default class {
   }
 
   placeShip(x, y, ship) {
+    const randomNumber = Math.floor(Math.random() * 2);
     const block = this.getBlock(x, y);
     block.placeShip(ship);
 
     this.#ships.push(ship);
-    this.placeHorizontally(x, y, ship);
+    this.placeVertically(x, y, ship, randomNumber);
   }
 
   createGaps(x, y, block) {
@@ -54,8 +55,7 @@ export default class {
     }
   }
 
-  placeHorizontally(x, y, ship) {
-    const randomNumber = Math.floor(Math.random() * 2);
+  placeHorizontally(x, y, ship, randomNumber) {
     const x_coordinates = this.alphabet.indexOf(x);
     for (let i = 0; i < ship.length; i++) {
       if (
@@ -89,6 +89,41 @@ export default class {
       .forEach((block) => this.createGaps(block.x, block.y, block));
   }
 
+  placeVertically(x, y, ship) {
+    const randomNumber = Math.floor(Math.random() * 2);
+    const x_coordinates = this.alphabet.indexOf(x);
+    for (let i = 0; i < ship.length; i++) {
+      if (
+        y === 1 && this.noShipInTheWay(x_coordinates, y, ship, "bottom")
+      )
+        this.spanShip(x_coordinates, y + i);
+
+        else if (
+          y === this.alphabet.length &&
+          this.noShipInTheWay(x_coordinates, y, ship, "top")
+        )
+          this.spanShip(x_coordinates, y - i);
+          else {
+            if(y + ship.length <= this.alphabet.length + 1 && this.noShipInTheWay(x_coordinates, y, ship, "bottom"))
+            {
+              if(this.noShipInTheWay(x_coordinates, y, ship, "top"))
+              {
+                randomNumber === 1 ? this.spanShip(x_coordinates, y + i) :
+                this.spanShip(x_coordinates, y - i);
+                continue;
+              }
+              this.spanShip(x_coordinates, y + i)
+            }
+            else if(this.noShipInTheWay(x_coordinates, y, ship, "top"))
+              this.spanShip(x_coordinates, y - i);
+            else return false;
+          }
+    }
+    this.filledBlocks
+    .filter((block) => block.grayBlocks.length === 0)
+    .forEach((block) => this.createGaps(block.x, block.y, block));
+  }
+
   spanShip(x, y)
   {
     const block = this.getBlock(this.alphabet[x], y);
@@ -103,8 +138,14 @@ export default class {
     if(direction === "right")
     targetBlock = this.getBlock(this.alphabet[x + ship.length - 1], y);
 
-    else
+    else if(direction === "left")
     targetBlock = this.getBlock(this.alphabet[x - ship.length + 1], y);
+
+    else if(direction === "bottom")
+    targetBlock = this.getBlock(this.alphabet[x], y + ship.length - 1);
+
+    else
+    targetBlock = this.getBlock(this.alphabet[x], y - ship.length + 1);
 
     return targetBlock.shipsAllowed && targetBlock.isEmpty;
   }
